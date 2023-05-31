35598991ed0aba857f7ca9ea65a4630dddf03c0a
gh120515 <123872197+gh120515@users.noreply.github.com>
2023-05-31 15:45:26 +0930

    server init connection

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 // import packages
 const express = require('express');
-
 // use the application
 const app = express();
 
 // create port (for app deployment to Heroku & local server)
 const PORT = process.env.PORT || 3001;
 
+// middleware to parse JSON body
+app.use(express.json());
+
+// route to handle fetching all notes (in JSON)
+require('./Develop/routes/api-routes.js')(app);
+
 // initialise server & display in console
 app.listen(PORT, () => {
-    console.log(`App listening at localhost${PORT}`);
+    console.log(`App listening at localhost ${PORT} (http://localhost:${PORT})`);
 });
